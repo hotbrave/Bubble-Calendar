557e0fd689ee9dec5765d899d6c1fa77fcc8cbe5
zyaaa <zybrave@gmail.com>
2024-10-03 21:01:44 +0800

    Initial Commit

---
+++ Bubble Calendar/Bubble_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_CalendarApp.swift
+//  Bubble Calendar
+//
+//  Created by Ethan on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
